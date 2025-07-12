e0f8b01e8fa327299d4019fc113a926e555855e1
pedroctag <96295155+pedroctag@users.noreply.github.com>
2025-07-11 21:51:59 -0300

    Update datapath1.v *Teste pendente*

--- ProjetoFinal/módulos/datapath1.v
+++ ProjetoFinal/módulos/datapath1.v
@@ -1,28 +1,46 @@
 module datapath1 (
     input clk,
-    input [31:0] PCBranch, in2 // entradas do mux do PC
-    output [31:0] out // Sinal Instr
+    input [31:0] PCBranch, PCPlus4 // entradas do mux do PC
+    input [4:0] WriteReg,
+    input [31:0] Result, // Valor da saída do mux com data memory e ALU
+    input PCSrc, // Sinal do módulo controlador PCSrc
+    input RegWrite, // Sinal do módulo controlador RegWrite
+    output [31:0] PC,
+    output [31:0] Instr,
     output [31:0] WriteData,
     output [31:0] SrcA
 );
-
-mux2x1_32bits mux (
+    wire [31:0] next_PC;
     
+mux2x1_32bits mux (
+    .inA(PCPlus4),
+    .inB(PCBranch),
+    .sel(PCSrc),
+    .out(next_PC)
 );
 
 PC pc(
-    .next_PC(in),
+    .clk(clk),
+    .next_PC(next_PC),
     .PC(PC)
 );
 
 instruction_memory im (
     .A(PC),
-    .RD(instr)
+    .RD(Instr)
 );
 
 register_file rf (
-    .A1(instr[25:21]),
-
+    .clk(clk),
+    .A1(Instr[25:21]),
+    .A2(Instr[20:16]),
+    .A3(WriteReg),
+    .WD3(Result),
+    .RD1(SrcA),
+    .RD2(WriteData),
+    .WE3(RegWrite)
 );
 
-endmodule// FAZENDO EM ORDEM, ESTE É O PRIMEIRO DATAPATH QUE APARECE, LER MAIS*/
+endmodule
+
+//Datapath azul da imagem de ref.
