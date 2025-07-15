ec7b7c0e210e0cf28980dbeb9d3d1c954e3f8fcd
aalouro <96149558+aalouro@users.noreply.github.com>
2025-07-15 20:19:09 -0300

    Update SignExtend.v

--- ProjetoFinal/módulos/SignExtend.v
+++ ProjetoFinal/módulos/SignExtend.v
@@ -3,8 +3,10 @@ module SignExtend(
     output [31:0] out
 );
 
-wire [15:0] bitone = 16'b1111111111111111;
-wire [15:0] bitzero = 16'b0000000000000000;
+//  wire [15:0] bitone = 16'b1111111111111111; 
+    wire [15:0] bitone = {16{1'b1}};
+//  wire [15:0] bitzero = 16'b0000000000000000;
+    wire [15:0] bitzero = {16{1'b0}};
 
 assign out = (in[15] == 1) ? {bitone, in} : {bitzero, in};
 
