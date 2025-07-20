73da4a833f7703061ed95d3f818767be4f6b1753
pedroctag <96295155+pedroctag@users.noreply.github.com>
2025-07-20 18:40:38 -0300

    Update instruction_memory.v

--- ProjetoFinal/módulos/instruction_memory.v
+++ ProjetoFinal/módulos/instruction_memory.v
@@ -7,11 +7,39 @@ reg [31:0] instruction [0:31]; // 32 espaços de instrução de 32 bits cada
 
 //espaços de memória para teste inicial    
 initial begin
-    instruction[4]  = 32'd4000;  // Endereço 4 recebe 4000
-    instruction[8]  = 32'd8000;  // Endereço 8 recebe 8000
-    instruction[16] = 32'd3200;  // Endereço 16 recebe 3200
-    instruction[20] = 32'd1000;  // Endereço 20 recebe 1000
-    instruction[24] = 32'd1500;  // Endereço 24 recebe 1500
+    instruction[0]  = 32'd4000;
+    instruction[1]  = 32'd4000;
+    instruction[2]  = 32'd4000;
+    instruction[3]  = 32'd4000;
+    instruction[4]  = 32'd4000;
+    instruction[5]  = 32'd4000; 
+    instruction[6]  = 32'd4000; 
+    instruction[7]  = 32'd4000; 
+    instruction[8]  = 32'd8000;
+    instruction[9]  = 32'd4000;
+    instruction[10]  = 32'd4000;
+    instruction[11]  = 32'd4000;
+    instruction[12]  = 32'd4000;
+    instruction[13]  = 32'd4000;
+    instruction[14]  = 32'd4000;
+    instruction[15]  = 32'd4000; 
+    instruction[16]  = 32'd4000; 
+    instruction[17]  = 32'd4000; 
+    instruction[18]  = 32'd8000;
+    instruction[19]  = 32'd4000;
+    instruction[20]  = 32'd4000;
+    instruction[21]  = 32'd4000;
+    instruction[22]  = 32'd4000;
+    instruction[23]  = 32'd4000;
+    instruction[24]  = 32'd4000;
+    instruction[25]  = 32'd4000; 
+    instruction[26]  = 32'd4000; 
+    instruction[27]  = 32'd4000; 
+    instruction[28]  = 32'd8000;
+    instruction[29]  = 32'd4000;
+    instruction[30]  = 32'd4000;
+    instruction[31]  = 32'd4000;
+
 end
 always @ (*)
 begin
