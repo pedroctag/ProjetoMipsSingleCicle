a74d719f11ccd48c74bed5458337928fd3af40e1
pedroctag <96295155+pedroctag@users.noreply.github.com>
2025-07-20 20:51:34 -0300

    Atualização de SignExtend.v *TEM DE TESTAR*

--- ProjetoFinal/módulos/SignExtend.v
+++ ProjetoFinal/módulos/SignExtend.v
@@ -1,13 +1,13 @@
 module SignExtend(
-    input [15:0] in,
+    input [11:0] in,
     output [31:0] out
 );
 
-//  wire [15:0] bitone = 16'b1111111111111111; 
-    wire [15:0] bitone = {16{1'b1}};
+    //  wire [15:0] bitone = 16'b1111111111111111; 
+    wire [19:0] bitone = {16{1'b1}};
 //  wire [15:0] bitzero = 16'b0000000000000000;
-    wire [15:0] bitzero = {16{1'b0}};
+    wire [19:0] bitzero = {16{1'b0}};
 
-assign out = (in[15] == 1) ? {bitone, in} : {bitzero, in};
+    assign out = (in[11] == 1) ? {bitone, in} : {bitzero, in};
 
 endmodule
