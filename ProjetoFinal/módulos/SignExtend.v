b06a0d8623f4827821ade8ddffb1f20d6b925aab
pedroctag <96295155+pedroctag@users.noreply.github.com>
2025-07-23 08:22:15 -0300

    SignExtend.v com instrução j *TEM DE TESTAR*

--- ProjetoFinal/módulos/SignExtend.v
+++ ProjetoFinal/módulos/SignExtend.v
@@ -10,6 +10,7 @@ begin
         2'b00 : out = {{20{in[31]}}, in[31:20]};
         2'b01 : out = {{20{in[31]}}, in[11:7]};
         2'b10 : out = {{20{in[31]}}, in[7], in[30:25], in[11:8], 1'b0};
+        2'b11 : out = {{12{Instr[31]}}, Instr[19:12], Instr[20], Instr[30:21], 1’b0};
         default: out = 32'bx;
     endcase
 end
